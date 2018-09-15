5c3d20c818e85a6b88337104636b6498d14b48b9
L3GO <kumiselorm12@gmial.com>
2018-09-15 21:43:23 +0000

    bug fixes: that make it work without a server

--- simpleLogin/main.js
+++ simpleLogin/main.js
@@ -6,6 +6,24 @@ var newlink = protocol + "//" + host + path;
 window.onload = init;
 
 function init() {
+  if (window.location.host == "" || window.location.host == null) {
+    offline();
+  } else {
+    online();
+  }
+}
+
+function offline() {
+  if (window.location.pathname.includes("signUp.html")) {
+    var signUp = document.getElementById("signUp");
+    signUp.onclick = signup;
+  } else if (window.location.pathname.includes("login.html")) {
+    var Login = document.getElementById("login");
+    Login.onclick = login;
+  }
+}
+
+function online() {
   if (window.location.pathname == "/simpleLogin/signUp.html") {
     var signUp = document.getElementById("signUp");
     signUp.onclick = signup;
@@ -28,15 +46,25 @@ function signup() {
       };
       let user = JSON.stringify(userObject);
       localStorage.setItem("userCredential", user);
-      console.log(localStorage);
+      if (window.location.host == "") {
+        let pathArray = window.location.pathname.split("/");
+        pathArray.pop();
+        pathArray.push("login.html");
+        let newPathname = "";
+        for (let index = 0; index < pathArray.length; index++) {
+          newPathname += "/";
+          newPathname += pathArray[index];
+        }
+        window.location.replace(protocol + "/" + newPathname);
+      } else {
+        path = "/simpleLogin/login.html";
+        let newlink = protocol + "//" + host + path;
+        window.location.replace(newlink);
+      }
     }
   } else {
     message.innerHTML = "please enter a valid email";
   }
-  path = "/simpleLogin/login.html";
-  let newlink = protocol + "//" + host + path;
-
-  window.location.replace(newlink);
 }
 
 function login() {
@@ -49,18 +77,45 @@ function login() {
   //check if user match exist
   if (matchUser == null || matchUser == "") {
     message.innerHTML = "please SignUp first";
+    let pathArray = window.location.pathname.split("/");
+    pathArray.pop();
+    pathArray.push("signUp.html");
     path = "/simpleLogin/signUp.html";
+    let newPathname = "";
+    for (let index = 0; index < pathArray.length; index++) {
+      newPathname += "/";
+      newPathname += pathArray[index];
+    }
     let newlink = protocol + "//" + host + path;
-    setTimeout(function() {
-      window.location.replace(newlink);
-    }, 10000);
+    if (window.location.host == "") {
+      setTimeout(function() {
+        window.location.replace(newPathname);
+      });
+    } else {
+      setTimeout(function() {
+        window.location.replace(newlink);
+      }, 10000);
+    }
   } else {
     // compare here
     if (email == matchUser.email) {
       if (password == matchUser.password) {
-        path = "/simpleLogin/landingPage.html";
-        let newlink = protocol + "//" + host + path;
-        window.location.replace(newlink);
+        // mactching route
+        if (window.location.host == "") {
+          var pathArray = window.location.pathname.split("/");
+          pathArray.pop();
+          pathArray.push("landingPage.html");
+          var newPathname = "";
+          for (let index = 0; index < pathArray.length; index++) {
+            newPathname += "/";
+            newPathname += pathArray[index];
+          }
+          window.location.replace(newPathname);
+        } else {
+          path = "/simpleLogin/landingPage.html";
+          let newlink = protocol + "//" + host + path;
+          window.location.replace(newlink);
+        }
       } else {
         message.innerHTML = "invalid email or password";
       }
